676b2f4dbfe8a93c4088e6a7b385c14c55609187
Alexander <axeltory@gmail.com>
2019-10-22 22:56:47 +0300

    First lesson

---
+++ core/di-tdd/Funq.Tests/ContainerFixture.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Funq.Tests
+{
+    [TestClass]
+    public class ContainerFixture
+    {
+        [TestMethod]
+        public void RegisterTypeAndGetInstance()
+        {
+            var container = new Container();
+
+            container.Register<IBar>(c => new Bar());
+
+            var bar = container.Resolve<IBar>();
+
+            Assert.IsNotNull(bar);
+            Assert.IsTrue(bar is Bar);
+        }
+
+        [TestMethod]
+        public void ResolveGetsDependenciesInjected(){
+            var container = new Container();
+
+            container.Register<IBar>(c => new Bar());
+            container.Register<IFoo>(c => new Foo(c.Resolve<IBar>()));
+
+            var foo = container.Resolve<IFoo>() as Foo;
+
+            Assert.IsNotNull(foo);
+            Assert.IsNotNull(foo.Bar);
+            Assert.IsTrue(foo is Foo);
+        }
+
+        public interface IBar { }
+        public interface IFoo { }
+
+        public class Bar : IBar { }
+        public class Foo : IFoo
+        {
+            public IBar Bar { get; private set; }
+
+            public Foo(IBar bar)
+            {
+                Bar = bar;
+            }
+        }
+    }
+}
---
+++ core/di-tdd/Funq/Container.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Funq
+{
+    public class Container{
+        private Dictionary<Type, object> _factories = new Dictionary<Type, object>();
+
+        public void Register<TService>(Func<Container, TService> factory){
+            _factories.Add(typeof(TService), factory);
+        }
+
+        public TService Resolve<TService>(){
+            object factory = _factories[typeof(TService)];
+            
+            return ((Func<Container, TService>)factory).Invoke(this);
+        }
+    }
+}
