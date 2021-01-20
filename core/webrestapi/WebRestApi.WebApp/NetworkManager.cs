99d1b4269f31d806c5188bbd0019d578db0948f1
Aliaksandr Makarchuk <Aliaksandr_Makarchuk@epam.com>
2021-01-20 14:27:00 +0300

    NetworkManager modified to use WebClient

--- core/webrestapi/WebRestApi.WebApp/NetworkManager.cs
+++ core/webrestapi/WebRestApi.WebApp/NetworkManager.cs
@@ -22,50 +22,50 @@ namespace WebRestApi.WebApp
             _networkConfiguration = networkConfiguration.Value;
         }
 
-        public async Task Connect()
-        {
-            using(var client = new WebClient())
-            {
-                try
-                {
-                    var test = await client.DownloadDataTaskAsync($"{_networkConfiguration.Api}/test");
-                }
-                catch (WebException ex)
-                {
-                    var exc = ex.Message;
-                }
-            }
-        }
-
         public async Task<bool> Login(string login, string password)
         {
             try
             {
+                var tokenUri = $"{_networkConfiguration.Api}/token";
                 var body = $"login={login}&password={password}";
-                // var requestBody = Encoding.UTF8.GetBytes(body);
-                var jsonBody = JsonSerializer.Serialize(new Creds{Login=login,Password=password});
+                var jsonBody = JsonSerializer.Serialize(new Creds { Login = login, Password = password });
                 var requestBody = Encoding.UTF8.GetBytes(jsonBody);
 
-                var request = WebRequest.Create($"{_networkConfiguration.Api}/token");
-                request.Method = "POST";
-                request.ContentType = "application/json";
-                request.ContentLength = requestBody.Length;
-
-                using(var stream = await request.GetRequestStreamAsync())
+                using(var client = new WebClient())
                 {
-                    await stream.WriteAsync(requestBody, 0, requestBody.Length);
-                }
-
-                using(var response = await request.GetResponseAsync())
-                {
-                    using(var stream = response.GetResponseStream())
+                    try
                     {
-                        using(var reader = new StreamReader(stream))
-                        {
-                            var res = await reader.ReadToEndAsync();
-                        }
+                        client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                        client.Headers.Add(HttpRequestHeader.ContentLength, requestBody.Length.ToString());
+                        var res = await client.UploadDataTaskAsync(tokenUri, "POST", requestBody);
+                        var res1 = Encoding.UTF8.GetString(res);
+                    }
+                    catch (WebException ex)
+                    {
+                        var res = ex.Message;
                     }
                 }
+
+                // var request = WebRequest.Create(tokenUri);
+                // request.Method = "POST";
+                // request.ContentType = "application/json";
+                // request.ContentLength = requestBody.Length;
+
+                // using(var stream = await request.GetRequestStreamAsync())
+                // {
+                //     await stream.WriteAsync(requestBody, 0, requestBody.Length);
+                // }
+
+                // using(var response = await request.GetResponseAsync())
+                // {
+                //     using(var stream = response.GetResponseStream())
+                //     {
+                //         using(var reader = new StreamReader(stream))
+                //         {
+                //             var res = await reader.ReadToEndAsync();
+                //         }
+                //     }
+                // }
             }
             catch (WebException ex)
             {
