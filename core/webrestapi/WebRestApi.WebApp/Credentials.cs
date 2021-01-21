072b62d0802fc7502a46c37a886bffe34003b6aa
Aliaksandr Makarchuk <Aliaksandr_Makarchuk@epam.com>
2021-01-21 18:36:58 +0300

    Refactoring

---
+++ core/webrestapi/WebRestApi.WebApp/Credentials.cs
@@ -0,0 +1,8 @@
+namespace WebRestApi.WebApp
+{
+    public class Credentials
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ core/webrestapi/WebRestApi/Models/Credentials.cs
@@ -0,0 +1,19 @@
+namespace WebRestApi.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Credentials
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
+        public string Login { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
+        public string Password { get; set; }
+    }
+}
