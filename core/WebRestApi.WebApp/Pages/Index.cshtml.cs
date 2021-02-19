47e33878f3a9b7c79e981b252d9a01fc5fbbf171
Aliaksandr Makarchuk <Aliaksandr_Makarchuk@epam.com>
2021-02-19 20:11:02 +0300

    Moved web app for webrestapi service out from sln

---
+++ core/WebRestApi.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebRestApi.WebApp.ViewModels;
+
+namespace WebRestApi.WebApp.Pages
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        public string Name { get; private set; }
+
+        [BindProperty]
+        public IList<Customer> Customers { get; set; }
+
+        public IndexModel()
+        {
+            Customers = new List<Customer>();
+        }
+
+        public IActionResult OnGet()
+        {
+            Name = User.Identity.Name;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            return await Task.FromResult(RedirectToPage());
+        }
+    }
+}
